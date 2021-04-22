17838ef2953945b0eb83907d516afddc5c20fd66
viktor-st-spb <viktor.st.spb@gmail.com>
2021-04-22 14:20:09 +0300

    The question model and questions added

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -1,14 +1,39 @@
 package com.example.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.Window
+import android.view.WindowManager
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        //Hide status bar
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+        /*** Button Start onClickListener ***/
+        btn_start.setOnClickListener {
+
+            // Check if EditText et_name is empty (no text inside this field)
+            if (et_name.text.toString().isEmpty()) {
+                Toast.makeText(
+                    this, "Please enter your name",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+            } else {
+                // In case user has entered his name - start QuizQuestionsActivity
+                val intent = Intent(this, QuizQuestionsActivity::class.java)
+                startActivity(intent)
+                // Destroy the current activity
+                finish()
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/quizapp/Question.kt
@@ -0,0 +1,21 @@
+package com.example.quizapp
+
+/****************************************/
+/*** This class defines the format of ***/
+/***      questions of an app         ***/
+/****************************************/
+
+data class Question(
+    // The id of the question
+    val id: Int,
+    // Question
+    val question: String,
+    val image: Int,
+    // Answers
+    val optionOne: String,
+    val optionTwo: String,
+    val optionThree: String,
+    val optionFour: String,
+    // Nhe number of the correct answer (1, 2, 3 or 4)
+    val correctAnswer: Int
+)
